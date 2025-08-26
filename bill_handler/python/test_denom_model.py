0a13a1810638cd63f48e9b1d9d45aeb9e1e50190
binsoy69 <binslegendary@gmail.com>
2025-08-26 14:47:09 +0800

    Added ncnn yolo models

--- bill_handler/python/test_denom_model.py
+++ bill_handler/python/test_denom_model.py
@@ -9,7 +9,7 @@ from ultralytics import YOLO
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Build the absolute path to the model
-model_path = os.path.join(script_dir, '..', 'models', 'denom-cls-v2.pt')
+model_path = os.path.join(script_dir, '..', 'models', 'denom-cls-v2_ncnn_model')
 
 if not os.path.exists(model_path):
     print(f"Model file not found at: {model_path}")
