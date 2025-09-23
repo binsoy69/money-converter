a485b5b3328b908f4ef42fa19d86e10495fc4133
binsoy69 <binslegendary@gmail.com>
2025-09-23 15:28:41 +0800

    rollback

--- bill_handler/python/test_servo.py
+++ bill_handler/python/test_servo.py
@@ -1,15 +1,31 @@
-from gpiozero import Servo
+import pigpio
 from time import sleep
 
-servo = Servo(21)
-val = -1
+SERVO_PIN = 21
+PUSH_ANGLE = 90
+RESET_ANGLE = 0
+DISPENSE_TIME = 0.02
+COUNT = 3
 
-try:
-	while True:
-		servo.value = val
-		sleep(0.1)
-		val = val + 0.1
-		if val > 1:
-			val = -1
-except KeyboardInterrupt:
-	print("Program stopped")
+# Servo pulse widths (microseconds) – adjust if needed
+MIN_PW = 500   # 0 degrees
+MAX_PW = 2500  # 180 degrees
+
+def angle_to_pulse(angle):
+    return int(MIN_PW + (angle / 180.0) * (MAX_PW - MIN_PW))
+
+pi = pigpio.pi()
+if not pi.connected:
+    exit()
+
+for i in range(COUNT):
+    for pos in range(PUSH_ANGLE, RESET_ANGLE - 1, -1):
+        pi.set_servo_pulsewidth(SERVO_PIN, angle_to_pulse(pos))
+        sleep(DISPENSE_TIME)
+    for pos in range(RESET_ANGLE, PUSH_ANGLE + 1):
+        pi.set_servo_pulsewidth(SERVO_PIN, angle_to_pulse(pos))
+        sleep(DISPENSE_TIME)
+    sleep(0.3)
+
+pi.set_servo_pulsewidth(SERVO_PIN, 0)  # turn off servo
+pi.stop()
