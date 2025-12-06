64dbc64f5d9a6eed058b2e3a6befd6e2aa564f2e
binsoy69 <binslegendary@gmail.com>
2025-12-06 15:29:44 +0800

    mod: acceptor speed

--- bill_handler/python/pi_bill_handler.py
+++ bill_handler/python/pi_bill_handler.py
@@ -492,7 +492,7 @@ class PiBillHandler:
     def accept_bill(
         self,
         required_denom: int,
-        motor_forward_ms: int = 800,
+        motor_forward_ms: int = 600,
         motor_reverse_ms: int = 1000,
         push_after_sort_ms: int = 1500,
         wait_for_ir_timeout_s: int = 60,
